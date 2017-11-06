e288e7120e0032655b04b8e6cd5d1062b7c6df87
MicaBanfi <micabanfi@hotmail.com>
2017-11-06 15:06:44 -0300

    Anda register

--- untitled/src/Views/Controller.java
+++ untitled/src/Views/Controller.java
@@ -21,4 +21,45 @@ package Views;
     }
 
 
-}*/
+}
+
+
+    public void possibleErrors(int error) {
+        if (error == 1) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Introduzca nombre de usuario y contrasena o registrese si no tiene usuario");
+            alert.setContentText(null);
+            alert.showAndWait();
+        }
+        else if(error==2){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Usuario o contrasena no encontrados");
+            alert.setContentText(null);
+            alert.showAndWait();
+        }
+        else if(error==3) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Complete todos los campos por favor");
+            alert.setContentText(null);
+            alert.showAndWait();
+        }
+        else if(error==4) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Usuario ya existente");
+            alert.setContentText(null);
+            alert.showAndWait();
+        }
+        else if(error==5) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("El viaje ya existe");
+            alert.setContentText(null);
+            alert.showAndWait();
+        }
+    }
+}
+*/
--- untitled/src/Views/Register.java
+++ untitled/src/Views/Register.java
@@ -20,52 +20,58 @@ import java.time.LocalDate;
 public class Register {
 
 
-    @FXML private TextField RgNametx;
-    @FXML private TextField RgSurnametx;
-    @FXML private TextField RgPhonetx;
-    @FXML private TextField RgCareertx;
-    @FXML private TextField RgUserNametx;
-    @FXML private TextField RgPasswordtx;
+    @FXML
+    private TextField RgNametx;
+    @FXML
+    private TextField RgSurnametx;
+    @FXML
+    private TextField RgPhonetx;
+    @FXML
+    private TextField RgCareertx;
+    @FXML
+    private TextField RgUserNametx;
+    @FXML
+    private TextField RgPasswordtx;
 
-   // @FXML private DatePicker RgBirthDate;
+    // @FXML private DatePicker RgBirthDate;
 
-    @FXML private ChoiceBox RgGenrech;
-    @FXML private ChoiceBox RgSmokech;
-    @FXML private ChoiceBox RgEatch;
+    @FXML
+    private ChoiceBox RgGenrech;
+    @FXML
+    private ChoiceBox RgSmokech;
+    @FXML
+    private ChoiceBox RgEatch;
 
-    public State estado= State.getInstance();
+    public State estado = State.getInstance();
 
-    public void newUser(){
-        String name=null, surname=null, career=null, phone=null, username1=null, password1=null,genre=null;
-        Boolean eat=null,smoke=null, emptyFields=true;
-        //LocalDate bDate=null;
+    public void newUser() {
+        boolean emptyFields;
+        emptyFields = checkRequestedFields();
+        if (!emptyFields) {
+            String name, surname, career, phone, username1, password1, genre;
+            Boolean eat = false, smoke = false;
+            name = RgNametx.getText();
+            surname = RgSurnametx.getText();
+            career = RgCareertx.getText();
+            phone = RgPhonetx.getText();
+            username1 = RgUserNametx.getText();
+            password1 = RgPasswordtx.getText();
+            genre = RgGenrech.getValue().toString();
 
-        name = RgNametx.getText();
-        surname = RgSurnametx.getText();
-        career = RgCareertx.getText();
-        phone = RgPhonetx.getText();
-        username1 = RgUserNametx.getText();
-        password1 = RgPasswordtx.getText();
-        genre = RgGenrech.getValue().toString();
-        //bDate = RgBirthDate.getValue();
+            String rta = new String(RgEatch.getValue().toString());
+            if (rta.compareTo("Si") == 0)
+                eat = true;
+            else eat = false;
+            rta = RgSmokech.getValue().toString();
+            if (rta.compareTo("Si") == 0)
+                smoke = true;
+            else smoke = false;
 
-        String rta = new String(RgEatch.getValue().toString());
-        if (rta.compareTo("Si") == 0)
-            eat = true;
-        else eat = false;
-        rta = RgSmokech.getValue().toString();
-        if (rta.compareTo("Si") == 0)
-            smoke = true;
-        else smoke = false;
-
-        Credential creddential1 = new Credential(username1, password1);
-        Person persona1 = new Person(name, surname, career, phone, smoke, eat, genre);
-        User usuario1 = new User(creddential1, persona1);
+            Credential creddential1 = new Credential(username1, password1);
+            Person persona1 = new Person(name, surname, career, phone, smoke, eat, genre);
+            User usuario1 = new User(creddential1, persona1);
 
             try {
-                if(name.isEmpty() || surname.isEmpty()|| career.isEmpty()|| phone.isEmpty()|| username1.isEmpty()|| password1.isEmpty()||genre.isEmpty()){
-
-                }
                 estado.register(usuario1);
                 //currentUser=new User(credential1,persona1);
                 Parent root2 = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
@@ -73,12 +79,7 @@ public class Register {
                 stage.setTitle("MainPage");
                 stage.setScene(new Scene(root2, 1000, 600));
                 stage.show();
-            }catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Usuario o contrasena no encontrados");
-                alert.setContentText(null);
-                alert.showAndWait()
+
             } catch (InvalidFields e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
@@ -88,49 +89,25 @@ public class Register {
             } catch (Exception e) {
                 System.out.println("Cant load mainPage");
             }
+
         }
-
-    public void IsEmpty() throws IsEmptyException{
-
-        RgNametx.getText().isEmpty() || RgSurnametx.getText().isEmpty()|| RgCareertx.getText().isEmpty()|| RgPhonetx.getText().isEmpty()|| RgUserNametx.getText().isEmpty()|| RgPasswordtx.getText().isEmpty())
-
     }
 
-    public void possibleErrors(int error) {
-        if (error == 1) {
+    public boolean checkRequestedFields(){
+        if(RgNametx.getText().isEmpty() || RgSurnametx.getText().isEmpty() ||
+                RgPhonetx.getText().isEmpty() ||
+                RgCareertx.getText().isEmpty() ||
+                RgUserNametx.getText().isEmpty() ||
+                RgPasswordtx.getText().isEmpty() ||
+                RgGenrech.getSelectionModel().isEmpty()||
+                RgSmokech.getSelectionModel().isEmpty()||
+                RgEatch.getSelectionModel().isEmpty() ){
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Introduzca nombre de usuario y contrasena o registrese si no tiene usuario");
-            alert.setContentText(null);
+            String content = "Por Favor llene todos los campos";
+            alert.setContentText(content);
             alert.showAndWait();
+            return true;
         }
-        else if(error==2){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Usuario o contrasena no encontrados");
-            alert.setContentText(null);
-            alert.showAndWait();
-        }
-        else if(error==3) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Complete todos los campos por favor");
-            alert.setContentText(null);
-            alert.showAndWait();
-        }
-        else if(error==4) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Usuario ya existente");
-            alert.setContentText(null);
-            alert.showAndWait();
-        }
-        else if(error==5) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("El viaje ya existe");
-            alert.setContentText(null);
-            alert.showAndWait();
-        }
+        return false;
     }
 }
